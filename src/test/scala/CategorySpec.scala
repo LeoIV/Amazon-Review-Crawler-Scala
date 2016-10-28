6e75fae442b21bc75e4cfb1e6e451403954626e6
Leonard Hövelmann <laeoiv@gmail.com>
2016-10-28 14:00:09 +0200

    added first test

---
+++ src/test/scala/CategorySpec.scala
@@ -0,0 +1,12 @@
+import de.leoiv.reviewcrawler.entities.Category
+import org.scalatest.{Matchers, FlatSpec, Suite}
+
+/**
+  * Created by hoevelmann on 28.10.2016.
+  */
+class CategorySpec extends FlatSpec with Matchers{
+  "A category" should "have a list of subcategories" in {
+    val category = new Category("Buecher", "https://www.amazon.de/b%C3%BCcher-buch-lesen/b/ref=sd_allcat_bo?ie=UTF8&node=186606", 2)
+    assert(category.subcategories.size > 0)
+  }
+}
